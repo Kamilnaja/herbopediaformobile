07163b57b32cd1ec9f99d3b8739aabf89f9d8aa1
Kamil Naja <kamilnaja@gmail.com>
2017-10-15 21:21:50 +0200

    initial commit

--- components/HerbsList.js
+++ components/HerbsList.js
@@ -6,7 +6,6 @@ render() {
 
     return (
       <View>
-        {console.log(Object.keys(this.props.herbsList))}
         <View>
           {
             Object.keys(this.props.herbsList).map((item, idx) => <Text>{this.props.herbsList[idx].name}</Text>)
